7ba48d07e43527644f8975bf9f663acfa34b36d2
mattxbart <mbartolome@sdcwa.org>
2017-09-26 10:42:46 -0700

    testing

--- set_motor_speed.py
+++ set_motor_speed.py
@@ -2,14 +2,16 @@
 from rplidar import RPLidar
 import sys
 import time
-from settings import PORT_NAME
+from settings import *
 
 def run(MOTOR_PWM):
     lidar = RPLidar(PORT_NAME)
-    time.sleep(1)
+    time.sleep(WAIT)
+    lidar.reset()
+    time.sleep(WAIT)
     lidar._motor_speed = MOTOR_PWM
     lidar.start_motor()
-    time.sleep(1)
+    time.sleep(WAIT)
     lidar.disconnect()
 
 if __name__ == '__main__':
